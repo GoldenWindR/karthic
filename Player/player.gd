3502cb90245571592b42e9e28f0071ceced465d0
PiotrKociolek <piotrkociolek2002@gmail.com>
2023-10-24 16:43:18 +0200

    Fixed loading Battlescene

--- Player/player.gd
+++ Player/player.gd
@@ -6,11 +6,15 @@ var shild = 0
 var health_progress_bar
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var canWalk = false
+var direction
 
 func _ready():
 	health_progress_bar = $ProgressBar
+	var currentScene = get_tree().get_current_scene()
+	if currentScene.get_name() =="world":
+		canWalk = true
 	
 func _process(delta):
 
@@ -35,23 +39,20 @@ func heal(amount):
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
 	
+	if canWalk:
+		if not is_on_floor():
+			velocity.y += gravity * delta
+			
+		direction = Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		move_and_slide()
 
 
 func _on_area_2d_body_entered(body):
 	get_tree().change_scene_to_file("res://FightScreen/FightScene.tscn")
-	print("weszłeś")
+	canWalk = false
+
