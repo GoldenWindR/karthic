e05883fc3954cf98c419747e58eaefb9d147051c
PiotrKociolek <piotrkociolek2002@gmail.com>
2023-11-20 21:34:16 +0100

    moving cards, funny error
    funny error during moving cards, all cards are selected on same time. Dropzone is not working

--- FightScreen/DropZone.gd
+++ FightScreen/DropZone.gd
@@ -1,15 +1,20 @@
-extends HBoxContainer
+extends Area2D
 
-@export var card: PackedScene = null
+var is_card_inside = false
+var entered_card = null
 
-func _can_drop_data(_at_position, _data):
-	return true
+# Funkcja wywoływana, gdy karta zostanie upuszczona na dropzone
+func _on_Card_area_entered(area):
+	if area is Sprite2D and not is_card_inside:
+		entered_card = area
+		is_card_inside = true
+		# Tutaj możesz dodać logikę obsługi upuszczenia karty
+		print("Karta upuszczona na dropzone:", entered_card.name)
 
-func _drop_data(_at_position, data):
-	var dropped_card = card.instantiate()
-	
-	add_child(dropped_card)
-	dropped_card.texture = data["texture"]
-	
-	CardActions.emit_signal("card_played", data["id"])
-
+# Funkcja wywoływana, gdy karta opuści dropzone
+func _on_Card_area_exited(area):
+	if area is Sprite2D and is_card_inside:
+		is_card_inside = false
+		# Tutaj możesz dodać logikę obsługi, gdy karta opuszcza dropzone
+		print("Karta opuściła dropzone:", entered_card.name)
+		entered_card = null
